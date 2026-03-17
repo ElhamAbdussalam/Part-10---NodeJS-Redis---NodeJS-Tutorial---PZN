4250587c63a0f9ecfc37a40b102db3250e3216d3
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-18 05:21:41 +0700

    set

--- test/redis.test.js
+++ test/redis.test.js
@@ -32,20 +32,35 @@ describe("redis", () => {
   //   expect(name).toBe(null);
   // });
 
-  it("should support list data structure", async () => {
-    await redis.del("names"); // 🔥 reset dulu
+  // it("should support list data structure", async () => {
+  //   await redis.del("names"); // 🔥 reset dulu
 
-    await redis.rpush("names", "Elham");
-    await redis.rpush("names", "Abdussalam");
-    await redis.rpush("names", "Muhammad");
+  //   await redis.rpush("names", "Elham");
+  //   await redis.rpush("names", "Abdussalam");
+  //   await redis.rpush("names", "Muhammad");
 
-    expect(await redis.llen("names")).toBe(3);
-    const names = await redis.lrange("names", 0, -1);
-    expect(names).toEqual(["Elham", "Abdussalam", "Muhammad"]);
+  //   expect(await redis.llen("names")).toBe(3);
+  //   const names = await redis.lrange("names", 0, -1);
+  //   expect(names).toEqual(["Elham", "Abdussalam", "Muhammad"]);
 
-    expect(await redis.lpop("names")).toBe("Elham");
-    expect(await redis.rpop("names")).toBe("Muhammad");
-    expect(await redis.llen("names")).toBe(1);
+  //   expect(await redis.lpop("names")).toBe("Elham");
+  //   expect(await redis.rpop("names")).toBe("Muhammad");
+  //   expect(await redis.llen("names")).toBe(1);
+
+  //   await redis.del("names");
+  // });
+
+  it("should support set data structure", async () => {
+    await redis.sadd("names", "Muhammad");
+    await redis.sadd("names", "Muhammad");
+    await redis.sadd("names", "Elham");
+    await redis.sadd("names", "Elham");
+    await redis.sadd("names", "Abdussalam");
+    await redis.sadd("names", "Abdussalam");
+
+    expect(await redis.scard("names")).toBe(3);
+    const names = await redis.smembers("names");
+    expect(names).toEqual(["Muhammad", "Elham", "Abdussalam"]);
 
     await redis.del("names");
   });
